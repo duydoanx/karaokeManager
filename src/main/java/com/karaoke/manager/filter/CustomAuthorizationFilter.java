9598cd085e9a686af3b4ba8b66a5d6e25c26cdcb
Duy Doan <duydoan1411@gmail.com>
2021-10-21 22:13:09 +0700

    fix datasource heroku

--- src/main/java/com/karaoke/manager/filter/CustomAuthorizationFilter.java
+++ src/main/java/com/karaoke/manager/filter/CustomAuthorizationFilter.java
@@ -2,6 +2,7 @@ package com.karaoke.manager.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import com.karaoke.manager.security.SecurityConstant;
@@ -49,7 +50,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
               new UsernamePasswordAuthenticationToken(username, null, authorities);
           SecurityContextHolder.getContext().setAuthentication(authenticationToken);
           filterChain.doFilter(request, response);
-        } catch (Exception e) {
+        } catch (JWTVerificationException e) {
           response.setStatus(HttpStatus.FORBIDDEN.value());
           response.setHeader("error", e.getMessage());
           Map<String, String> body = new HashMap<>();
