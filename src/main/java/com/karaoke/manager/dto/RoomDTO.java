97d0a4af897df8f8919ae3f4db1ee942a3bc1820
duydoanx <duydoan1411@gmail.com>
2021-11-20 03:22:20 +0700

    Fix: fix validate when create and update room.

--- src/main/java/com/karaoke/manager/dto/RoomDTO.java
+++ src/main/java/com/karaoke/manager/dto/RoomDTO.java
@@ -1,8 +1,15 @@
 package com.karaoke.manager.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.karaoke.manager.entity.Room;
+import com.karaoke.manager.entity.RoomType;
+import com.karaoke.manager.validation.annotation.StringContains;
+import com.karaoke.manager.validation.group.room.CreateRoom;
+import com.karaoke.manager.validation.group.room.UpdateRoom;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @Getter
 @Setter
@@ -11,9 +18,19 @@ public class RoomDTO {
   @JsonProperty(access = JsonProperty.Access.READ_ONLY)
   private Long id;
 
+  @NotBlank(groups = {CreateRoom.class})
   private String name;
 
+  @StringContains(
+      groups = {UpdateRoom.class},
+      listString = {Room.ENABLE, Room.DISABLE},
+      message = "Invalid statusCode.")
   private String statusCode;
 
+  @StringContains(
+      groups = {CreateRoom.class, UpdateRoom.class},
+      listString = {RoomType.VIP, RoomType.NORMAL},
+      message = "Invalid type.")
+  @NotBlank(groups = {CreateRoom.class})
   private String type;
 }
---
+++ src/main/java/com/karaoke/manager/validation/group/room/CreateRoom.java
@@ -0,0 +1,3 @@
+package com.karaoke.manager.validation.group.room;
+
+public interface CreateRoom {}
